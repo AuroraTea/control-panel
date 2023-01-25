a1527fd7e90aeb8a72caef64ee894bd1eb835b70
AuroraTea <1352685369@qq.com>
2023-01-25 18:24:18 +0800

    ⚙ ignore web dist in server/

--- server/static/web/_nuxt/composables.27806f98.js
+++
@@ -1,1 +0,0 @@
-import{aR as a}from"./entry.78b45273.js";function s(e,u){return a()._useHead(e,u)}export{s as u};
--- server/static/web/_nuxt/ipv4.da6f114a.js
+++
@@ -1,1 +0,0 @@
-import c from"./IPv4.6898d752.js";import{aO as r,o as t,c as e}from"./entry.78b45273.js";import"./index.5d4a6269.js";const n={};function _(s,a){const o=c;return t(),e(o)}const i=r(n,[["render",_]]);export{i as default};
